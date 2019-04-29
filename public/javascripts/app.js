fee2d61d943141cd0f1e363f36848dc138d76a8a
Zezhong Yu <yuzez@seas.upenn.edu>
2019-04-29 11:39:43 -0400

    yzz

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,5 @@
-var app = angular.module('angularjsNodejsTutorial', []);
+var app = angular.module('angularjsNodejsTutorial', ['nvd3']);
+
 app.controller('loginController', function($scope, $http) {
   $scope.verifyLogin = function() {
     // To check in the console if the variables are correctly storing the input:
@@ -82,6 +83,170 @@ app.controller('dashboardController', function($scope, $http) {
     
   };
 });
+
+app.controller('top100avgController', function($scope, $http) {
+  // normal variables
+    //console.log('submit here')
+    var request = $http.get('/top100_avg');
+    var even=[];
+    var odd=[];
+
+    request.success(function(response) {
+      // success
+      // console.log('response');
+      //console.log(response);
+      //$scope.randomMovies=response;
+      var rows=response;
+
+      for(var i=0;i<rows.length;i++){
+        rows[i]['id']=i+1;       
+      }
+
+      console.log(rows);
+
+      $scope.top100avg=rows;
+    });
+    request.error(function(err) {
+      // failed
+      console.log("error: ", err);
+    });
+    
+    $scope.search = function() {
+    // To check in the console if the variables are correctly storing the input:
+    // console.log($scope.username, $scope.password);
+
+    var search_request = $http({
+      url: '/search',
+      method: "POST",
+      data: {
+        'name': $scope.college_name,
+      }
+    })
+    
+    search_request.success(function(response) {
+      // success
+      // console.log('response');
+      console.log(response);
+      if (response.result === "success") {
+        // After you've written the INSERT query in routes/index.js, uncomment the following line
+        //window.location.href = "http://localhost:8081/dashboard"
+        window.location.href = "/college?cid="+response.cid;
+      }else{
+        alert("No such university. Please check the spell.");
+      }
+    });
+
+    search_request.error(function(err) {
+      // failed
+      console.log("error: ", err);
+    });
+    }
+});
+
+app.controller('top100growController', function($scope, $http) {
+  // normal variables
+    //console.log('submit here')
+    var request = $http.get('/top100_grow');
+
+    request.success(function(response) {
+      // success
+      // console.log('response');
+      //console.log(response);
+      //$scope.randomMovies=response;
+      var rows=response;
+
+      for(var i=0;i<rows.length;i++){
+        rows[i]['id']=i+1;       
+      }
+
+      console.log(rows);
+
+      $scope.top100grow=rows;
+    });
+    request.error(function(err) {
+      // failed
+      console.log("error: ", err);
+    });    
+});
+
+
+app.controller('rankplot', function($scope, $http) {
+  // normal variables
+    //console.log('submit here')
+    /**
+    var cname=document.getElementById('cname').innerHTML;
+    var request = $http({
+      url: '/plot_data',
+      method: "POST",
+      data: {
+        'cname': cname
+      }
+    })**/
+
+    $scope.options = {
+            chart: {
+                type: 'lineChart',
+                height: 450,
+                margin : {
+                    top: 20,
+                    right: 20,
+                    bottom: 40,
+                    left: 55
+                },
+                x: function(d){ return d.x; },
+                y: function(d){ return d.y; },
+                useInteractiveGuideline: true,
+                dispatch: {
+                    stateChange: function(e){ console.log("stateChange"); },
+                    changeState: function(e){ console.log("changeState"); },
+                    tooltipShow: function(e){ console.log("tooltipShow"); },
+                    tooltipHide: function(e){ console.log("tooltipHide"); }
+                },
+                xAxis: {
+                    axisLabel: 'Time (ms)'
+                },
+                yAxis: {
+                    axisLabel: 'Voltage (v)',
+                    tickFormat: function(d){
+                        return d3.format('.02f')(d);
+                    },
+                    axisLabelDistance: -10
+                },
+                callback: function(chart){
+                    console.log("!!! lineChart callback !!!");
+                }
+            },
+            title: {
+                enable: true,
+                text: 'Title for Line Chart'
+            },
+            subtitle: {
+                enable: true,
+                text: 'Subtitle for simple line chart. Lorem ipsum dolor sit amet, at eam blandit sadipscing, vim adhuc sanctus disputando ex, cu usu affert alienum urbanitas.',
+                css: {
+                    'text-align': 'center',
+                    'margin': '10px 13px 0px 7px'
+                }
+            },
+            caption: {
+                enable: true,
+                html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec<sup>[1, <a href="https://github.com/krispo/angular-nvd3" target="_blank">2</a>, 3]</sup>.',
+                css: {
+                    'text-align': 'justify',
+                    'margin': '10px 13px 0px 7px'
+                }
+            }
+        };
+
+        $scope.data = [{
+          values:[{x:1999,y:1,series:0},{x:1999,y:1,series:0},{x:1999,y:1,series:0}],
+          key:"test", color="#2ca02c"
+        }];
+
+
+     
+});
+
 
 // Template for adding a controller
 /*
